14ba667144a249045bff7cd22b3f445d699a14f5
César <mottacesar001@gmail.com>
2022-01-21 12:20:29 -0300

    mate

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcoes-nativas-matematicas</title>
+</head>
+<body>
+
+
+
+<?php
+
+
+     $num = 8.5;
+
+    echo ceil($num).'<br>'; // ceil - arredondamento para cima
+
+    echo floor($num).'<br>'; // floor  - arredondamento para baixo
+
+    echo round($num).'<br>'; // round- arredondamento com base na fração
+    
+    echo rand(10,20).'<br>'; // geral valor aleatorio
+
+    echo sqrt(9);
+
+
+
+?>
+    
+</body>
+</html>
